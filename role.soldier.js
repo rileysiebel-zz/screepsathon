9f22bd9e1efe748602541f996f34fb87a0c5e2fb
Kasden Bunker <kasden.bunker@c3iot.ai>
2018-11-08 17:00:31 -0800

    four more roles

---
+++ role.soldier.js
@@ -0,0 +1,16 @@
+var roleSoldier = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        if(creep.memory.building) {
+            var targets = creep.room.find(FIND_HOSTILE_CREEPS);
+            if(targets.length) {
+                if(creep.rangedAttack(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+        }
+    }
+};
+
+module.exports = roleSoldier;
