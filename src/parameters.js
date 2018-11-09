f49df19cf2d058149099d48759c4921098fca2e8
c3NicolasHenry <nicolas.henry@c3iot.ai>
2018-11-08 20:50:05 -0800

    Forward prop

--- src/parameters.js
+++ src/parameters.js
@@ -1,15 +1,50 @@
+
+
+
 var parameters = {
-  parameters: function() {
-    return {
-      harvester: 1 / 7,
-      builder: 1 / 7,
-      upgrader: 1 / 7,
-      claimer: 1 / 7,
-      archer: 1 / 7,
-      healer: 1 / 7,
-      soldier: 1 / 7
-    }
-  }
+
+    parameters: function() {
+        return [1/7, 1/7, 1/7, 1/7, 1/7, 1/7, 1/7]
+    },
+
+    forwardPropagation: function (inputs, model) {
+        //Make one vector of inputs:
+
+        //Load parameters from model:
+        var w1 = model['W1'],
+            b1 = model['b1'],
+            w2 = model['W2'],
+            b2 = model['b2'],
+            w3 = model['W3'],
+            b3 = model['b3'];
+        //Do the first Linear step:
+        var z1 =  util.add(util.multiply(inputs,w1),b1);
+        //Put it through the first activation function:
+        var a1 = util.activate(z1);
+
+        //Second linear step
+        var z2 = util.add(util.multiply(a1,w2), b2);
+        var a2 = util.activate(z3);
+
+        var z3 = util.add(util.multiply(a2,w3), b3);
+        var a3 = util.activate(z3);
+
+
+        var output = softmax(a3)
+
+
+
+        return output;
+
+        }
+
+
+
+
+
+
 };
+
+
 
 module.exports = parameters;
--- src/util.js
+++ src/util.js
@@ -1,15 +1,78 @@
 var util = {
 
     cleanupCreeps: function() {
-      
+
       for (var name in Memory.creeps) {
         if (!Game.creeps[name]) {
           delete Memory.creeps[name];
           console.log('Clearing non-existing creep memory:', name);
         }
        }
-      
+
+    },
+
+    multiply: function (a, b) {
+    var aNumRows = a.length, aNumCols = a[0].length,
+        bNumRows = b.length, bNumCols = b[0].length,
+        m = new Array(aNumRows);  // initialize array of rows
+    for (var r = 0; r < aNumRows; ++r) {
+      m[r] = new Array(bNumCols); // initialize the current row
+      for (var c = 0; c < bNumCols; ++c) {
+        m[r][c] = 0;             // initialize the current cell
+        for (var i = 0; i < aNumCols; ++i) {
+          m[r][c] += a[r][i] * b[i][c];
+        }
+      }
     }
+    return m;
+  },
+
+  add: function (a, b) {
+  var aNumRows = a.length, aNumCols = a[0].length,
+      bNumRows = b.length, bNumCols = b[0].length,
+      m = new Array(aNumRows);  // initialize array of rows
+  for (var r = 0; r < aNumRows; ++r) {
+    m[r] = new Array(aNumCols); // initialize the current row
+    for (var c = 0; c < aNumCols; ++c) {
+      m[r][c] = a[r][c] + b[r][c];             // initialize the current cell
+
+    }
+  }
+  return m;
+},
+
+activate(a){
+    var aNumRows = a.length, aNumCols = a[0].length;
+      m = new Array(aNumRows);
+      for (var r = 0; r < aNumRows; ++r) {
+        m[r] = new Array(aNumCols); // initialize the current row
+        for (var c = 0; c < aNumCols; ++c) {
+          m[r][c] = Math.tanh(a[r][c])
+                       // initialize the current cell
+          }
+       }
+       return m;
+     },
+
+function softmax(arr) {
+         return arr.map(function(value,index) {
+           return Math.exp(value) / arr.map( function(y /*value*/){ return Math.exp(y) } ).reduce( function(a,b){ return a+b })
+         })
+     }
+
+
+
+
+
+  return
+}
+
+
+
+
+
+
+
 };
 
 module.exports = util;
